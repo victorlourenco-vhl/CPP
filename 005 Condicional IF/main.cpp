a28192f81a7a72e3791ba8221f5d3045e4205a96
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-03-16 11:38:31 -0300

    LEARN: Condiconal IF

---
+++ 005 Condicional IF/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dinheiro = 20;
+
+    if(dinheiro > 10)
+        cout << "Eu vou ao cinema!" << endl;
+    else
+        cout << "Eu NAO vou ao cinema!" << endl;
+        
+    return 0;
+}
