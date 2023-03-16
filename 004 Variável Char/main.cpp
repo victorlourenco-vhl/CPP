72c814067e30363990238bc55d26d7f105b65cc6
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-03-16 11:25:24 -0300

    LEARN: Declarando variáveis do tipo char

---
+++ 004 Variável Char/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char c = 'o';
+    char d = 105;
+
+    cout << c << d << endl;
+    cout << c + d << endl;
+    
+    return 0;
+}
